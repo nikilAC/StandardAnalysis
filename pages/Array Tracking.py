8a89c3aa6c3ac782f56343bd131313df6c4d220c
Nikil Sunku <nikil.sunku@aircapture.co>
2024-09-17 10:14:16 -0700

    Update requirements

--- pages/Array Tracking.py
+++ pages/Array Tracking.py
@@ -4,14 +4,9 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
-import matplotlib.pyplot as plt
 from plotly.subplots import make_subplots
-import itables 
-from st_aggrid import AgGrid
 
 
-# SQL Connection module
-import pyodbc
 
 # Set Page Con
 st.set_page_config(page_title="General Analysis Dashboard", layout="wide")
